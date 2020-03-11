ffc173753318177c335b3f60b419019057a720a2
Fredrik Rafn <groombeskeder@gmail.com>
2020-03-11 13:47:01 +0100

    Initial commit

---
+++ Line Shuffler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Line_Shuffler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any button when 'combo.txt' is in the same folder as this program...");
+            Console.ReadKey();
+
+            var lines = File.ReadAllLines("combo.txt");
+            var rnd = new Random();
+            lines = lines.OrderBy(line => rnd.Next()).ToArray();
+            File.WriteAllLines("shuffled_combo.txt", lines);
+
+            Console.WriteLine("Check your folder for 'shuffled_combo.txt'!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Line-Shuffler
+ Takes a text document and shuffles the lines of it.
